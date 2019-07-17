02ec188e2bce8d257cc02c6ac22414422b4dc1b3
Kyle Mabaso <kyle@insytive.co.za>
2019-07-17 02:50:04 +0200

    An even better profile presentation

--- app/src/main/java/com/example/thejourney/AboutActivity.java
+++ app/src/main/java/com/example/thejourney/AboutActivity.java
@@ -3,7 +3,9 @@ package com.example.thejourney;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -15,11 +17,18 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("About Andela");
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_about);
 
         webView=findViewById(R.id.aboutAndela);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://en.wikipedia.org/wiki/Andela");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+
+        webView.loadUrl("https://andela.com/alc/");
     }
 }
